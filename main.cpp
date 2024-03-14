a3dafd5cb40b19547207bfe1d4884836608e814f
hyf <no>
2024-03-13 21:35:19 -0500

    adding new test images

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@
 
 int main(){
 	
-    std::string filepath        = "./images/orange.png";
+    std::string filepath        = "./images/minion_loss.png";
     std::string new_filepath    = "./images/orange_copy.png";
 
 	PNG image ( filepath );
    
     image.print_ihdr();
     //image.print_pixels();
-
+    image.write_file( new_filepath );
     
 
 	return 0;
